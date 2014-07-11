5b3516b057b3ad0cc0156582a31db5000e81fe78
My Ho <mydiemho@gmail.com>
2014-07-11 00:20:24 -0700

    invites tab correctly displayed user's pending invites

--- app/src/main/java/com/sms/partyview/fragments/PendingEventsFragment.java
+++ app/src/main/java/com/sms/partyview/fragments/PendingEventsFragment.java
@@ -1,6 +1,18 @@
 package com.sms.partyview.fragments;
 
+import com.parse.FindCallback;
+import com.parse.ParseException;
+import com.parse.ParseQuery;
+import com.parse.ParseUser;
+import com.sms.partyview.AttendanceStatus;
+import com.sms.partyview.models.Event;
+import com.sms.partyview.models.EventUser;
+
 import android.app.Fragment;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by myho on 7/3/14.
@@ -14,6 +26,31 @@ public class PendingEventsFragment extends EventListFragment {
 
     @Override
     protected void populateEventList() {
-        // do nothing for now
+        // Define the class we would like to query
+        ParseQuery<EventUser> query = ParseQuery.getQuery(EventUser.class);
+
+        // Define our query conditions
+
+        // get list of events where user
+        query.whereEqualTo("user", ParseUser.getCurrentUser());
+        query.whereEqualTo("status", AttendanceStatus.INVITED.toString());
+        query.addAscendingOrder("date");
+        query.include("event");
+
+        query.findInBackground(
+                new FindCallback<EventUser>() {
+                    @Override
+                    public void done(List<EventUser> eventUsers, ParseException e) {
+
+                        Log.d("DEBUG", "invited eventUsers");
+                        Log.d("DEBUG", eventUsers.size() + eventUsers.toString());
+                        List<Event> events = new ArrayList<Event>();
+                        for (EventUser eventUser : eventUsers) {
+                            events.add(eventUser.getEvent());
+                        }
+                        eventAdapter.addAll(events);
+                    }
+                }
+        );
     }
 }
