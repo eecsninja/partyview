64325ddc4c1c188387c85899a1ce7e4b9e61e9ed
Simon Que <simonque@gmail.com>
2014-07-12 10:37:29 -0700

    Remove unused createEvent() in NewEventActivity
    It now resides in the EditEventFragment.

--- app/src/main/java/com/sms/partyview/activities/NewEventActivity.java
+++ app/src/main/java/com/sms/partyview/activities/NewEventActivity.java
@@ -22,7 +22,6 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.app.NavUtils;
 import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
 import android.view.Window;
 
 import java.util.List;
@@ -61,10 +60,6 @@ public class NewEventActivity extends FragmentActivity implements EventSaverInte
             default:
                 return super.onOptionsItemSelected(item);
         }
-    }
-
-    public void createEvent(View view) {
-        mEditEventFragment.createEvent(view);
     }
 
     @Override
