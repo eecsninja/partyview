9cbf9dd8e6833c8dd460e3d9675c7260c95145f4
My Ho <mydiemho@gmail.com>
2014-07-13 21:55:53 -0700

    fix NPE when returning from EditEvent view

--- app/src/main/java/com/sms/partyview/activities/EditEventActivity.java
+++ app/src/main/java/com/sms/partyview/activities/EditEventActivity.java
@@ -10,7 +10,9 @@ import com.sms.partyview.models.LocalEvent;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.util.Log;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import java.util.List;
@@ -41,6 +43,21 @@ public class EditEventActivity extends NewEventActivity {
     }
 
     @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        switch (item.getItemId()) {
+            // Respond to the action bar's Up/Home button
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
     public void saveNewEvent(final Event event, final String invitesString) {
         // Saving an existing Parse object will update it in the remote database.
         // TODO: Pass the updated event back to the parent activity.
@@ -60,24 +77,24 @@ public class EditEventActivity extends NewEventActivity {
         query.include("host");
 
         query.findInBackground(
-            new FindCallback<Event>() {
-                @Override
-                public void done(List<Event> events, ParseException e) {
-                    // Handle exceptions or if there was no event found.
-                    String toastMsg = null;
-                    if (e != null) {
-                        toastMsg = e.getMessage();
-                    } else if (events.isEmpty()) {
-                        toastMsg = "No event with that ID was found.";
-                    }
-                    if (toastMsg != null) {
-                        showToastAndFinish(toastMsg);
-                    }
+                new FindCallback<Event>() {
+                    @Override
+                    public void done(List<Event> events, ParseException e) {
+                        // Handle exceptions or if there was no event found.
+                        String toastMsg = null;
+                        if (e != null) {
+                            toastMsg = e.getMessage();
+                        } else if (events.isEmpty()) {
+                            toastMsg = "No event with that ID was found.";
+                        }
+                        if (toastMsg != null) {
+                            showToastAndFinish(toastMsg);
+                        }
 
-                    // Store the event.
-                    loadEventForEditing(events.get(0));
+                        // Store the event.
+                        loadEventForEditing(events.get(0));
+                    }
                 }
-            }
         );
     }
 
