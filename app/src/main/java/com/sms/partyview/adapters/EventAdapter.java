8a471824d98a0fad868c545f84fc8943b126f5d8
Simon Que <simonque@gmail.com>
2014-07-20 14:32:07 -0700

    Revert "Set a different color for each event title"
    This reverts commit a181a77dbafb9efe55b91c5c27549d71f826472a.
    
    Conflicts:
    	app/src/main/java/com/sms/partyview/adapters/EventAdapter.java

--- app/src/main/java/com/sms/partyview/adapters/EventAdapter.java
+++ app/src/main/java/com/sms/partyview/adapters/EventAdapter.java
@@ -13,7 +13,6 @@ import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
@@ -24,26 +23,9 @@ import static com.sms.partyview.helpers.Utils.DISPLAY_DATE_TIME_FORMATTER;
  * Created by sque on 7/4/14.
  */
 public class EventAdapter extends ArrayAdapter<Event> {
-    // A list of possible colors for event titles.
-    ArrayList<Integer> mEventTitleColors;
 
     public EventAdapter(Context context, List<Event> event_list) {
         super(context, 0, event_list);
-
-        // Populate the color list with potential colors.
-        mEventTitleColors = new ArrayList<Integer>();
-        mEventTitleColors.add(R.color.blue);
-        mEventTitleColors.add(R.color.navy);
-        mEventTitleColors.add(R.color.green);
-        mEventTitleColors.add(R.color.teal);
-        mEventTitleColors.add(R.color.maroon);
-        mEventTitleColors.add(R.color.purple);
-        mEventTitleColors.add(R.color.olive);
-        mEventTitleColors.add(R.color.dark_red);
-        mEventTitleColors.add(R.color.light_blue);
-        mEventTitleColors.add(R.color.light_green);
-        mEventTitleColors.add(R.color.gold);
-        mEventTitleColors.add(R.color.orange);
     }
 
     @Override
@@ -63,7 +45,6 @@ public class EventAdapter extends ArrayAdapter<Event> {
                 (TextView) view.findViewById(R.id.tvEventItemTime);
         TextView hostNameField =
                 (TextView) view.findViewById(R.id.tvEventItemHost);
-
         // Set view content.
         Event event = getItem(position);
         titleField.setText(event.getTitle());
@@ -73,16 +54,6 @@ public class EventAdapter extends ArrayAdapter<Event> {
         // TODO: should show a display name.
 
         hostNameField.setText("Hosted by: " + event.getHost().getUsername());
-
-        // Set a color for the title based on a hash of the title.
-        int hash = event.getTitle().hashCode();
-        int color = getColorFromHash(hash);
-        // If the color is the same as the previous color, get the next color.
-        if (position > 0 &&
-            getColorFromHash(getItem(position - 1).getTitle().hashCode()) == color) {
-            color = getColorFromHash(hash + 1);
-        }
-        titleField.setTextColor(color);
 
         // Date/time should be of a lighter color.
         timeField.setTextColor(getContext().getResources().getColor(R.color.gray));
@@ -117,15 +88,5 @@ public class EventAdapter extends ArrayAdapter<Event> {
                 return e1.getStartDate().compareTo(e2.getStartDate());
             }
         });
-    }
-
-    // Selects an event title color based on a hash value.
-    private int getColorFromHash(int hash) {
-        int colorIndex = hash % mEventTitleColors.size();
-        // Just in case the result is negative.
-        while (colorIndex < 0) {
-            colorIndex += mEventTitleColors.size();
-        }
-        return getContext().getResources().getColor(mEventTitleColors.get(colorIndex));
     }
 }
