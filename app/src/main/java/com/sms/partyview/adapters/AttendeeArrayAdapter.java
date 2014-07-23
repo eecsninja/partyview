159d1947a9bab8b868e544ef906804b25a2e20fb
sandramngo <sandramngo@gmail.com>
2014-07-23 01:08:52 -0700

    add status circles to attendee list

--- app/src/main/java/com/sms/partyview/adapters/AttendeeArrayAdapter.java
+++ app/src/main/java/com/sms/partyview/adapters/AttendeeArrayAdapter.java
@@ -8,6 +8,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.List;
@@ -39,7 +40,23 @@ public class AttendeeArrayAdapter extends ArrayAdapter<Attendee> {
         // Set view content.
         Attendee attendee = getItem(position);
         tvAttendeeName.setText(attendee.getUsername());
-        tvAttendeeStatus.setText("<" + attendee.getStatus().toString() + ">");
+        tvAttendeeStatus.setText("(" + attendee.getStatus().toString().toLowerCase() + ")");
+
+        LinearLayout statusIcon = (LinearLayout) view.findViewById(R.id.llStatusIcon);
+        switch(attendee.getStatus()) {
+            case ACCEPTED:
+                statusIcon.setBackgroundResource(R.drawable.status_accepted_circle);
+                break;
+            case PRESENT:
+                statusIcon.setBackgroundResource(R.drawable.status_present_circle);
+                break;
+            case DECLINED:
+                statusIcon.setBackgroundResource(R.drawable.status_declined_circle);
+                break;
+            case INVITED:
+                statusIcon.setBackgroundResource(R.drawable.status_invited_circle);
+                break;
+        }
         return view;
     }
 }
